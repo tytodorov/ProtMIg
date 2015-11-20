b09d2a157ea8468ac4ae6fb0a3f9f8447105b845
Ice User5 <iceuser5@telerik.local>
2015-11-20 09:44:47 +0000

    Changes made by Ice User5 (iceuser5@telerik.local).

--- Old_app/js/index.js
+++ Old_app/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {CHANGES
+var app = {CHANGES22231313
     // Application Constructor
     initialize: function() {
         this.bindEvents();
